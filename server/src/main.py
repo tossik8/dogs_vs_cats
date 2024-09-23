767a54a08d840d44f7d836a36fc962b9798dad88
Nikita <toropov.nikita2004@gmail.com>
2024-09-23 12:35:40 +0200

    chore: retrieve allowed origins from .env

--- server/src/main.py
+++ server/src/main.py
@@ -1,16 +1,21 @@
+import os
+
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+load_dotenv()
 import cv2
 import numpy as np
 import tensorflow as tf
 
 model = tf.keras.models.load_model('../model.keras')
+allowed_origins = os.getenv('ALLOWED_ORIGINS').split(',')
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=allowed_origins
 )
 
 @app.post("/images")
